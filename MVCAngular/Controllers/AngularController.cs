252224ed12983035d65fd7424cc985c99e4c02ed
Jhenifs <jhenisantana62@gmail.com>
2016-08-26 19:54:56 -0300

    1.0

---
+++ MVCAngular/Controllers/AngularController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAngular.Controllers
+{
+    public class AngularController : Controller
+    {
+        // GET: Angular
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
